dccdad35024ab24bfbbd731b6c091a8f04489d82
Jake Plimack <Jake.plimack@gmail.com>
2014-03-12 19:00:22 -0600

    php is so stupid for so many reasons, better make emacs smarter

--- emacs.el
+++ emacs.el
@@ -1065,6 +1065,35 @@ SCHEDULED: %^t
 (add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("Makefile\\.PL$" . cperl-mode))
 
+(require 'php-mode)
+(require 'flymake)
+
+(defun flymake-php-init()
+  "Use php to check the syntax of the current file."
+  (let* ((temp (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+         (local (file-relate-name temp (file-name-directory buffer-file-name))))
+    (list "php" (list "-f" local "-1"))))
+
+(add-to-list 'flymake-err-line-patterns
+  '("\\(Parse\\|Fatal\\) error: +\\(.*?\\) in \\(.?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+
+(add-to-list 'flymake-allowed-file-name-masks '("\\.php$" flymake-php-init))
+
+(add-hook 'php-mode-hook (lambda () (flymake-mode 1)))
+(define-key php-mode-map '[M-S-up] 'flymake-goto-prev-error)
+(define-key php-mode-map '[M-S-down] 'flymake-goto-next-error)
+
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+(setq
+  c-basic-offset 4
+  tab-width 4
+  )
+
+(eval-after-load 'php-mode
+  '(progn
+     (define-key php-mode-map (kbd "RET") 'reindent-then-newline-and-indent)))
+
 (defun jp:python-mode-defaults ()
   (run-hooks 'jp:prog-mode-hook))
 
